578eb5663ea02297e6a1a2e31491eff81831d9df
Aahad Ali <104500845+Aahad-Ali@users.noreply.github.com>
2022-12-28 22:20:51 +0500

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "*"],
+    origin: ["https://context-api-with-jwt.cyclic.app/", "*"],
     credentials: true,
   })
 );
